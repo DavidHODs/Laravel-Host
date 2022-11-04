e9936e230ce6626d8f0b166037ed66ec21a07bc2
DavidHODs <davidoluwatobi41@gmail.com>
2022-11-04 15:53:45 +0100

    found a fix for the path bug

--- ops.sh
+++ ops.sh
@@ -73,13 +73,13 @@ function gitOp {
     # checks if remote origin exists or if existing remote origin is not the same as the laravel folder to be pulled
     if ! git remote -v; then
         sudo git init
-        sudo git remote add origin https://${key}@github.com/DavidHODs/laravel-realworld-example-app.git
+        sudo git remote add origin https://`cat /home/ubuntu/Laravel-Host/.key`@github.com/DavidHODs/laravel-realworld-example-app.git
     else
-        if ! git ls-remote --exit-code https://${key}@github.com/DavidHODs/laravel-realworld-example-app.git; then 
+        if ! git ls-remote --exit-code https://`cat /home/ubuntu/Laravel-Host/.key`@github.com/DavidHODs/laravel-realworld-example-app.git; then 
             # removes origin and clears the contents of AltExam 
             sudo git remote rm origin
             sudo rm -rf ~/AltExam/{*,.*}
-            sudo git remote add origin https://${key}@github.com/DavidHODs/laravel-realworld-example-app.git
+            sudo git remote add origin https://`cat /home/ubuntu/Laravel-Host/.key`@github.com/DavidHODs/laravel-realworld-example-app.git
         fi
     fi
  
